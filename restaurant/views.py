8e293ec72995e356a0961a7cf3aa4f0ee3893fd4
Muhammad-Afzaal1 <afzaal4517muhammad@gmail.com>
2025-06-20 11:29:36 +0500

    Booking and menu api endpoints are created

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 # Create your views here.
+def sayHello(request):
+    return HttpResponse("hello, world")
+
+def index(request):
+    return render(request, 'restaurant/index.html',{})
+
+class MenuView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class MenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
